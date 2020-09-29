c5a3ee808b14b48b20a4552635a92a5232c004ed
Samuel Zapote Granjeno <samuel@zapote.org>
2020-09-28 23:52:08 -0500

    feat(ChristmasTree): Created the pot mesh

--- src/scenes/ChristmasScene/christmas-tree.group.ts
+++ src/scenes/ChristmasScene/christmas-tree.group.ts
@@ -1,18 +1,23 @@
-import { Group, MeshStandardMaterial } from "three";
+import { CylinderGeometry, Group, Mesh, MeshStandardMaterial } from "three";
+import Decoration from "./decoration.group";
 
 class ChristmasTree extends Group {
 	private potMaterial: MeshStandardMaterial;
+	private pot: Mesh;
 
 	constructor() {
 		super();
-		
-		this.potMaterial = new MeshStandardMaterial( {
+		this.potMaterial = new MeshStandardMaterial({
 			color: 0xf97514,
 			flatShading: true,
 			metalness: 0,
 			roughness: 0.8,
 			refractionRatio: 0.25
-	} );
+		});
+		this.pot = new Mesh(
+			Decoration.addNoise(new CylinderGeometry(30, 25, 35, 8, 2), 2),
+      this.potMaterial,
+		);
 	}
 }
 
